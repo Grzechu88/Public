4f0073a30de309305485b4133a4853e682f73b32
Grzechu88 <GrzegorzSzczesny88@gmail.com>
2020-10-29 19:34:19 +0100

    Notes - menu
    Added the file menu with first options.
    
    New note & quit already work. The rest is a work in progress.

--- Notes/notes.py
+++ Notes/notes.py
@@ -12,6 +12,36 @@ root = Tk()
 root.title("Notes")
 root.geometry("300x300")
 
+global mainText, newText
+newText = ""
+
+def newNote():
+    newWindow = Toplevel()
+
+    newWindow.title("Notes")
+    newWindow.geometry("300x300")
+
+    #create scroll bar
+    scrolly = Scrollbar(newWindow)
+    scrolly.pack(side=RIGHT, fill=Y)
+    #create text box and fill the entire window with it
+    textBox = Text(newWindow, bg="magenta")
+    textBox.columnconfigure(0, weight=1)
+    textBox.pack(expand=TRUE, fill=BOTH)
+    #attach the scrollbar to the text box
+    textBox.config(yscrollcommand=scrolly.set)
+    scrolly.config(command=textBox.yview)
+    
+    newText = textBox.get("1.0", "end-1c")
+    
+    newNote.mainloop()
+    
+def saveNotes():
+    mainText = textBox.get("1.0", "end-1c")
+    print("Text from the main window: " + str(mainText))
+    if newText is True:
+        print("Text from the secondwindow: " + str(newText))
+    
 #create scroll bar
 scrolly = Scrollbar(root)
 scrolly.pack(side=RIGHT, fill=Y)
@@ -22,5 +52,21 @@ textBox.pack(expand=TRUE, fill=BOTH)
 #attach the scrollbar to the text box
 textBox.config(yscrollcommand=scrolly.set)
 scrolly.config(command=textBox.yview)
+
+
+#adding the menu widget
+topMenu = Menu(root)
+fileMenu = Menu(topMenu, tearoff=0)
+fileMenu.add_command(label="New note", command=newNote)
+fileMenu.add_command(label="Save notes", command=saveNotes)
+fileMenu.add_separator()
+fileMenu.add_command(label="New category")
+fileMenu.add_command(label="Category list")
+fileMenu.add_separator()
+fileMenu.add_command(label="Exit", command=root.destroy)
+        
+topMenu.add_cascade(label="File", menu=fileMenu)
+
+root.config(menu=topMenu)
 
 root.mainloop()
